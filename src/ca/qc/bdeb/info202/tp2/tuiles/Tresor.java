4bf62c4db36ce8f92fde9796d81dc1ef0f7799bc
thier <thierrychamp15@gmail.com>
2021-04-28 15:27:59 -0400

    alo

--- src/ca/qc/bdeb/info202/tp2/tuiles/Tresor.java
+++ src/ca/qc/bdeb/info202/tp2/tuiles/Tresor.java
@@ -4,8 +4,14 @@ import ca.qc.bdeb.info202.tp2.objets.Objet;
 
 public class Tresor extends Tuile {
     private Objet obj;
+    private boolean estOuvert = false;
     public Tresor(int x, int y, Objet obj) {
         super(x, y, '$', true);
         this.obj = obj;
+    }
+
+    @Override
+    public char getSymbole() {
+        return estOuvert ? '_' : super.symbole;
     }
 }
